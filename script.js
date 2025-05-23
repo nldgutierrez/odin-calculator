b82d6d21fb1c811bad080ce467d8b9720590782c
Noelle Gutierrez <113011161+nldgutierrez@users.noreply.github.com>
2025-05-23 10:01:24 +0800

    Create operate function

--- script.js
+++ script.js
@@ -7,3 +7,29 @@ function divide(a, b) {
     if (a === 0 || b === 0) return 'ERROR';
     return a / b;
 }
+
+// Variables
+
+let value1;
+let value2;
+let operator;
+
+// Operate Function
+function operate(operator, value1, value2) {
+    switch (operator) {
+        case '+':
+            add(value1, value2);
+            break;
+        case '-':
+            subtract(value1, value2);
+            break;
+        case '*':
+            multiply(value1, value2);
+            break;
+        case '/':
+            divide(value1, value2);
+            break;
+        default:
+            console.log('Operation failed');
+    }
+}
